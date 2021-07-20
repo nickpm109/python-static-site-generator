3a188d37c3f0adca1d83f5356a140767e450767f
nickpm109 <87670410+nickpm109@users.noreply.github.com>
2021-07-20 09:58:16 -0400

    Module 2 update 2

--- ssg/site.py
+++ ssg/site.py
@@ -17,7 +17,7 @@ class Site:
             if path.is_dir():
                 self.create_dir(path)
             elif path.is_file():
-                self.run_parsers(path)
+                self.run_parser(path)
 
     def load_parser(self, extension):
         for parser in self.parsers:
@@ -25,7 +25,7 @@ class Site:
                 return parser
 
     def run_parser(self, path):
-        parser = self.load_parsers(path.suffix)
+        parser = self.load_parser(path.suffix)
         if parser is not None:
             parser.parse(path, self.source, self.dest)
         else:
